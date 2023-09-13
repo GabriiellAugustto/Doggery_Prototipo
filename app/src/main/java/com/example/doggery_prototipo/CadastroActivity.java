7b1895344e885aa1d7c8b2bffdedfc21400a62e6
GabriiellAugustto <gabriel.gauitarra.05@gmail.com>
2023-09-13 17:25:02 -0300

    New

---
+++ app/src/main/java/com/example/doggery_prototipo/CadastroActivity.java
@@ -0,0 +1,14 @@
+package com.example.doggery_prototipo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_layout);
+    }
+}
